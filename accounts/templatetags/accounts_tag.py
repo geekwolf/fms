b19c0c3b6d6499d616b586e0d764ca898c26d14c
geekwolf <xibeilangy@163.com>
2018-03-02 14:16:01 +0800

    Add uuid convert to str filter

--- accounts/templatetags/accounts_tag.py
+++ accounts/templatetags/accounts_tag.py
@@ -1,26 +1,38 @@
 # -*- encoding:utf-8 -*-
-from django import template 
+from django import template
 
 register = template.Library()
 
-@register.filter()  
+
+@register.filter()
 def usetostr(value):
 
-	data = []
-	for v in value:
-		data.append("%s(%s)"%(v.username,v.fullname))
+    data = []
+    for v in value:
+        data.append("%s(%s)" % (v.username, v.fullname))
 
-	if data:
-		data = (',').join(data)
-	return data
+    if data:
+        data = (',').join(data)
+    return data
 
-@register.filter()  
+
+@register.filter()
 def gsetostr(value):
 
-	data = []
-	for v in value:
-		data.append(v.name)
+    data = []
+    for v in value:
+        data.append(v.name)
 
-	if data:
-		data = (',').join(data)
-	return data
+    if data:
+        data = (',').join(data)
+    return data
+
+
+@register.filter()
+def split(value):
+    return str(value).split('-')[0]
+
+
+@register.filter()
+def uuid_to_str(value):
+    return ('').join(str(value).split('-'))
